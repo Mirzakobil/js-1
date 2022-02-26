ce465225a906a2b8274ff82c6d307844778d1e03
Mirzakobil <mirzakobilanvarov@gmail.com>
2022-02-27 01:47:20 +0500

    task3 done

--- hw2/task3.js
+++ hw2/task3.js
@@ -1,8 +1,18 @@
 const arr =  ["НПП Прізвище Кіл-ть Приблизно","______________________в_ базіпо_Україні", "0-rt-rgfr", "8484fjdfkf", "МЕЛЬНИК","ШЕВЧЕНКО","БОЙКО","КОВАЛЕНКО","БОНДАРЕНКО","ТКАЧЕНКО","КОВАЛЬЧУК","КРАВЧЕНКО","ОЛІЙНИК"]
-const invalidValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!"]
-for(letter of arr){
-    console.log(letter)
-    if (invalidValue.includes(letter)){
-        console.log(1)
+const invalidValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "_"]
+
+
+for(str of arr){
+    for(letter of str){
+        if (invalidValue.includes(letter)){
+            console.log(arr.indexOf(str))
+            console.log(str)
+            console.log(letter)
+            const index = arr.indexOf(str);
+            delete arr[index]
+            // arr.splice(index, 1)
+            break;
+        }
     }
 }
+console.log(arr)
